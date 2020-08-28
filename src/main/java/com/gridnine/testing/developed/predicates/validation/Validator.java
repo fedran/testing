cbafb3e4534ed971652f8a72e289ea19c1193b89
fedan <fedor.anikiev@yandex.ru>
2020-08-28 18:50:09 +0300

    one stream instead two in validator

--- src/main/java/com/gridnine/testing/developed/predicates/validation/Validator.java
+++ src/main/java/com/gridnine/testing/developed/predicates/validation/Validator.java
@@ -14,19 +14,15 @@ public class Validator implements FlightPredicate {
             throw new ValidationException("Null in arguments of test method");
         } else if (flight.getSegments() == null) {
             throw new ValidationException("Flight segments is null");
-        } else if (flight.getSegments()
-                .stream()
-                .anyMatch(Objects::isNull)) {
+        } else if (flight.getSegments().stream().anyMatch(Objects::isNull)) {
             throw new ValidationException("One of flights segment  is null");
-        } else if (flight.getSegments()
-                .stream()
-                .anyMatch(this::segmentValidate)) {
-            throw new ValidationException("ArrivalDate is before DepartureDate");
         }
+        flight.getSegments()
+                .forEach(segment -> {
+                    if (segment.getDepartureDate().isAfter(segment.getArrivalDate())) {
+                        throw new ValidationException("ArrivalDate is before DepartureDate");
+                    }
+                });
         return true;
-    }
-
-    private boolean segmentValidate(Segment segment) {
-        return segment.getDepartureDate().isAfter(segment.getArrivalDate());
     }
 }
