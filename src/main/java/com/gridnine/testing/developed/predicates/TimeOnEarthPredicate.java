a17116f995979031e6cbc8a69d63aaa97fa16c10
fedan <fedor.anikiev@yandex.ru>
2020-08-30 14:56:23 +0300

    filtering if more than 2 hour between all segments

--- src/main/java/com/gridnine/testing/developed/predicates/TimeOnEarthPredicate.java
+++ src/main/java/com/gridnine/testing/developed/predicates/TimeOnEarthPredicate.java
@@ -2,21 +2,23 @@ package com.gridnine.testing.developed.predicates;
 
 import com.gridnine.testing.initial.Flight;
 
+import java.time.temporal.ChronoUnit;
 import java.time.LocalDateTime;
 
 public class TimeOnEarthPredicate implements FlightPredicate {
     @Override
     public boolean test(Flight flight) {
+        long minutes = 0;
         for (int i = 0; i + 1 < flight.getSegments().size(); i++) {
             LocalDateTime arrDate = flight.getSegments()
                     .get(i)
                     .getArrivalDate();
             LocalDateTime depDate = flight.getSegments()
                     .get(i + 1)
-                    .getDepartureDate()
-                    .minusHours(2L);
-            if (arrDate.isBefore(depDate)) return false;
+                    .getDepartureDate();
+
+            minutes += arrDate.until(depDate, ChronoUnit.MINUTES);
         }
-        return true;
+        return minutes <= 120L;
     }
 }
--- src/test/java/com/gridnine/testing/TimeOnEarthPredicateTest.java
+++ src/test/java/com/gridnine/testing/TimeOnEarthPredicateTest.java
@@ -5,57 +5,58 @@ import com.gridnine.testing.initial.Segment;
 import com.gridnine.testing.initial.Flight;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TimeOnEarthPredicateTest {
     @Test
-    public void timeOnEarthFilterTest() {
+    public void predicateNullTest() {
+        assertThrows(NullPointerException.class, () -> {
+            TimeOnEarthPredicate predicate = new TimeOnEarthPredicate();
+            predicate.test(null);
+        });
+    }
+
+    @Test
+    public void predicateTrueTest(){
         Segment segment1 = new Segment(LocalDateTime.now(),
                 LocalDateTime.now().plusHours(1L));
         Segment segment2 = new Segment(LocalDateTime.now().plusHours(2L),
                 LocalDateTime.now().plusHours(3L));
-        Segment segment3 = new Segment(LocalDateTime.now().plusHours(6L),
-                LocalDateTime.now().plusHours(7L));
+        Segment segment3 = new Segment(LocalDateTime.now().plusHours(4L),
+                LocalDateTime.now().plusHours(5L));
 
-        List<Segment> segmentList1 = new ArrayList<>();
-        segmentList1.add(segment1);
-        segmentList1.add(segment2);
-        segmentList1.add(segment3);
+        List<Segment> segmentList = new ArrayList<>();
+        segmentList.add(segment1);
+        segmentList.add(segment2);
+        segmentList.add(segment3);
 
-        Flight f1 = new Flight(segmentList1);
-
-        Segment segment4 = new Segment(LocalDateTime.now(),
-                LocalDateTime.now().plusHours(1L));
-        Segment segment5 = new Segment(LocalDateTime.now().plusHours(2L),
-                LocalDateTime.now().plusHours(3L));
-        Segment segment6 = new Segment(LocalDateTime.now().plusHours(4L),
-                LocalDateTime.now().plusHours(7L));
-
-        List<Segment> segmentList2 = new ArrayList<>();
-        segmentList2.add(segment4);
-        segmentList2.add(segment5);
-        segmentList2.add(segment6);
-
-        Flight f2 = new Flight(segmentList2);
-
-        System.out.println(f2);
-
-        List<Flight> flightList = new ArrayList<>();
-        flightList.add(f1);
-        flightList.add(f2);
+        Flight flight = new Flight(segmentList);
 
         TimeOnEarthPredicate predicate = new TimeOnEarthPredicate();
-        flightList = flightList.stream()
-                .filter(predicate)
-                .collect(Collectors.toList());
+        assertTrue(predicate.test(flight));
+    }
 
-        assertFalse(flightList.contains(f1));
-        assertTrue(flightList.contains(f2));
+    @Test
+    public void predicateFalseTest() {
+        Segment segment1 = new Segment(LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1L));
+        Segment segment2 = new Segment(LocalDateTime.now().plusHours(2L),
+                LocalDateTime.now().plusHours(3L));
+        Segment segment3 = new Segment(LocalDateTime.now().plusHours(4L).plusMinutes(1L),
+                LocalDateTime.now().plusHours(5L));
+
+        List<Segment> segmentList = new ArrayList<>();
+        segmentList.add(segment1);
+        segmentList.add(segment2);
+        segmentList.add(segment3);
+
+        Flight f = new Flight(segmentList);
+
+        TimeOnEarthPredicate predicate = new TimeOnEarthPredicate();
+        assertFalse(predicate.test(f));
     }
 }
